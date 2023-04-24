805cdbed07106b6a72fc8ea39210064da7625a45
Ishi Tran <berugen1st@gmail.com>
2023-04-24 23:21:25 +0700

    add function boilerplate

---
+++ src/key_mapping.rs
@@ -0,0 +1,3 @@
+pub fn use_vim_keymapping() {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,41 @@
 pub mod manager;
 pub mod query;
 pub mod reader;
+pub mod app_layout;
 
-fn main() {}
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    Terminal
+};
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+fn main() -> Result<(), io::Error> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        app_layout::ui(f);
+    })?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
